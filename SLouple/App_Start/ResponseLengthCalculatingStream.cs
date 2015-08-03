f9fdc679a80a0cf20e7d965a1c8f5349aaf8ab23
Cosh_ <oo0SPiCa0oo@gmail.com>
2015-08-03 04:53:32 -0700

    Added Content Length Stream

---
+++ SLouple/App_Start/ResponseLengthCalculatingStream.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace SLouple.App_Start
+{
+    public class ResponseLengthCalculatingStream : MemoryStream
+    {
+        private readonly Stream responseStream;
+        public long responseSize = 0;
+        public ResponseLengthCalculatingStream(Stream responseStream)
+        {
+            this.responseStream = responseStream;
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            this.responseSize += count;
+            this.responseStream.Write(buffer, offset, count);
+        }
+
+        public override void Flush()
+        {
+            var responseSize = this.responseSize;
+            base.Flush();
+        }
+    }
+}
