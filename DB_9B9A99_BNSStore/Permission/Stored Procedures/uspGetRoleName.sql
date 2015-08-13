f0833ce8c54a01efef179743f5e3524313ed2288
Cosh_ <oo0SPiCa0oo@gmail.com>
2015-08-13 03:52:25 -0700

    Added uspGetRoleName

---
+++ DB_9B9A99_BNSStore/Permission/Stored Procedures/uspGetRoleName.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE Permission.uspGetRoleName
+	@RoleID smallint = NULL,
+	@RoleName varchar(100) = NULL OUTPUT
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	
+	SET @RoleName = (SELECT RoleName FROM [Permission].[Role] WHERE RoleID = @RoleID)
+
+END
