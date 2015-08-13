c392622ba7bea51c0ae7845d18b747887061ed8d
Cosh_ <oo0SPiCa0oo@gmail.com>
2015-08-13 04:55:28 -0700

    Added GetUserID

--- DB_9B9A99_BNSStore/User/Stored Procedures/uspGetUserID.sql
+++ DB_9B9A99_BNSStore/User/Stored Procedures/uspGetUserID.sql
@@ -1,38 +1,22 @@
-﻿-- =============================================
--- Author:		Cosh_
--- Create date: 2014.9.13
--- Description:	Create simple user
--- =============================================
-CREATE PROCEDURE [User].[uspGetUserID] 
+﻿CREATE PROCEDURE [User].[uspGetUserID] 
 	@UserID int = NULL OUTPUT,
 	@Username varchar(100) = NULL,
 	@EmailAddress varchar(254) = NULL
 
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-
-    -- Insert statements for procedure here
 
 	IF @Username IS NOT NULL
 	BEGIN
-		SET @UserID = (SELECT TOP 1 [UserID] FROM [User].Account WHERE Username = @Username)
+		SET @UserID = (SELECT [UserID] FROM [User].Account WHERE Username = @Username)
 	END
 	ELSE IF @EmailAddress IS NOT NULL
 	BEGIN
-		SET @UserID = (SELECT TOP 1 [UserID] FROM [User].Email WHERE EmailAddress = @EmailAddress)
+		SET @UserID = (SELECT [UserID] FROM [User].Email WHERE EmailAddress = @EmailAddress)
 	END
 	IF @UserID IS NULL
 	BEGIN
 			SET @UserID = -1;
 	END
 END
-
-
-GO
-GRANT EXECUTE
-    ON OBJECT::[User].[uspGetUserID] TO [db_executor]
-    AS [dbo];
-
---
+++ DB_9B9A99_BNSStore/User/Stored Procedures/uspGetUsername.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [User].[uspGetUsername] 
+	@UserID int = NULL ,
+	@Username varchar(100) = NULL OUTPUT
+
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SET @Username = (SELECT [Username] FROM [User].Account WHERE UserID = @UserID)
+END
