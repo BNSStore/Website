9bccd4ddfe353202de449f1efee2a5a16a2c1e41
Cosh_ <oo0SPiCa0oo@gmail.com>
2015-08-13 04:09:22 -0700

    Added Policy and Role

---
+++ SLouple/Models/Shared/Policy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SLouple.MVC.Shared
+{
+    public class Policy
+    {
+        private int? policyID;
+        private string policyName;
+
+        public Policy(int policyID)
+        {
+            this.policyID = policyID;
+        }
+
+        public Policy(string policyName)
+        {
+            this.policyName = policyName;
+        }
+
+        public int GetPolicyID(){
+            if (policyID == null)
+            {
+                SqlStoredProcedures sqlSP = new SqlStoredProcedures();
+                policyID = sqlSP.PermissionGetPolicyID(policyName);
+            }
+            return policyID.Value;
+        }
+
+        public string GetPolicyName()
+        {
+            if (policyName == null)
+            {
+                SqlStoredProcedures sqlSP = new SqlStoredProcedures();
+                policyName = sqlSP.PermissionGetPolicyName(policyID.Value);
+            }
+            return policyName;
+        }
+    }
+}
---
+++ SLouple/Models/Shared/Role.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SLouple.MVC.Shared
+{
+    public class Role
+    {
+        private int? roleID;
+        private string roleName;
+
+        public Role(int roleID)
+        {
+            this.roleID = roleID;
+        }
+
+        public Role(string roleName)
+        {
+            this.roleName = roleName;
+        }
+
+        public int GetRoleID(){
+            if (roleID == null)
+            {
+                SqlStoredProcedures sqlSP = new SqlStoredProcedures();
+                roleID = sqlSP.PermissionGetRoleID(roleName);
+            }
+            return roleID.Value;
+        }
+
+        public string GetRoleName()
+        {
+            if (roleName == null)
+            {
+                SqlStoredProcedures sqlSP = new SqlStoredProcedures();
+                roleName = sqlSP.PermissionGetRoleName(roleID.Value);
+            }
+            return roleName;
+        }
+    }
+}
